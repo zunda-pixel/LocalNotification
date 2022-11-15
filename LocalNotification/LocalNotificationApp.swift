6c3817ae17e444c2fee27a83e9134d7dbed34e29
zunda <zunda.dev@gmail.com>
2022-11-15 16:23:15 +0900

    Initial Commit

---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by zunda on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
